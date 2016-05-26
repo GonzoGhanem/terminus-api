a981874cf6568f11bc76a2c1e77484aefe614e48
Gonzo <gonzalo.ghanem@bonzzu.com>
2016-05-25 22:38:59 -0300

    Fixed bug of timezone

--- app/serializers/bondi_serializer.rb
+++ app/serializers/bondi_serializer.rb
@@ -3,11 +3,11 @@ class BondiSerializer < ActiveModel::Serializer
   attributes :name, :departure, :arrival, :today, :days
   
   def departure
-    object.departure.strftime "%H:%M"
+    object.departure.getutc.strftime "%H:%M"
   end
   
   def arrival
-    object.arrival.strftime "%H:%M"
+    object.arrival.getutc.strftime "%H:%M"
   end
   
   def today
