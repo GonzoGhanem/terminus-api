fdf80c4cac768b4128f7ef045aa001d0dbb40a79
Gonzo <gonzalo.ghanem@bonzzu.com>
2016-05-27 09:16:25 -0300

    Test to see if timezone works

--- app/models/bondi.rb
+++ app/models/bondi.rb
@@ -1,6 +1,6 @@
 class Bondi < ActiveRecord::Base
   
   scope :from_to, ->(origin,destination) { where("origin = ? AND destination = ?", origin, destination) }
-  scope :next_three_from_now, -> { where('departure > ?', Time.current).first(3) } 
+  scope :next_three_from_now, -> { where('departure > ?', Time.current.getutc).first(3) } 
 
 end
