6ef434ae902d5b00c079d388dd84810b5245f113
Gonzo <gonzalo.ghanem@bonzzu.com>
2016-05-28 17:19:15 -0300

    Added saturday to schedule update_bondis

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every :sunday, :at => '12:00am' do
   rake 'fetch_bondis'
 end
 
-every [:monday, :tuesday, :wednesday, :thursday, :friday] do
+every [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday] do
   rake 'update_bondis'
 end
