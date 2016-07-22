65a22bc15df5faabea37db443cab8f344bc2f88f
Gonzo <gonzalo.ghanem@bonzzu.com>
2016-07-21 22:54:11 -0300

    Fixed issue of bondis order

--- app/models/bondi.rb
+++ app/models/bondi.rb
@@ -1,6 +1,6 @@
 class Bondi < ActiveRecord::Base
   
-  scope :from_to, ->(origin,destination) { where("origin = ? AND destination = ?", origin, destination) }
+  scope :from_to, ->(origin,destination) { where("origin = ? AND destination = ?", origin, destination).order(departure: :desc) }
   scope :next_three_from_now, -> { where('departure > ?', Time.current).first(3) } 
 
   def route_details
